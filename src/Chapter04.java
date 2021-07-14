f687850fdf04fce53955a7712074077eb000f83d
kmtszky <kmtszk711+IT@gmail.com>
2021-07-14 22:30:50 +0900

    second commit

---
+++ src/Chapter04.java
@@ -0,0 +1,12 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		final int LUCKY_NUMBER = 7;
+		String words = "yuki";
+		System.out.println(words);
+		
+		words = "snow";
+		System.out.println(words);
+		System.out.println(LUCKY_NUMBER);
+	}
+}
